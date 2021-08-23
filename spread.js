85c9818ac712af6907a5482601e8550af79d22ca
unknown <mobinh264@gmail.com>
2021-08-23 13:55:45 +0600

    Spread Oparator usign and work in Two dimantional array

---
+++ spread.js
@@ -0,0 +1,27 @@
+const numbers = [ 23, 26, 74, 92, 16 ];
+// console.log(numbers)
+// console.log(...numbers); ==> ai 3(...) diye array ar upadan ta ber kra jai ja number hisabe dei 
+
+const max = Math.max(65 , 51, 541);
+// console.log(max);
+/*
+ const maxInArray = Math.max(numbers); ==> ai ta output dekhabe na krn hoche j Math.max sudhu number k niye kaj kre array k na tai aitar output ta dibe an */
+
+ /* 
+This is the way to find array big number using(...);
+const maxInArray = Math.max(...numbers);
+console.log(maxInArray) */
+
+/* 
+const numbers2 = [ 23, 26, 74, 92, 16, 48, 52 ];
+const numbers3 = [ 412 , ...numbers, ...numbers2 , 45 ];
+console.log(numbers3) */
+
+/*
+ const num = [ 440, 561, 41, 96, 46 ];
+const twoDimantionalArray = [ 52, 61, 32, 41, 56, 12 , ...[num] ];
+const twoDimantionalArrayWithSpread = [ 52, 61, 32, 41, 56, 12 , ...num ];
+console.log(twoDimantionalArray) */
+
+
+
